f27650d4600486fd369326ea096051f0ef7b4a0d
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-09-02 14:11:32 +0530

    final commit
    last commit

--- BlogItAll/home/views.py
+++ BlogItAll/home/views.py
@@ -5,25 +5,31 @@ from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
+        print('authenticated')
         return render(request, 'index.html')
     else:
-        return redirect('loginUser')
+        print('not authenticated')
+        return redirect('/loginUser')
 
 def loginUser(request):
     if request.method=='POST':
-        username=request.POST.get('email')
+        username=request.POST.get('username')
         password=request.POST.get('password')
+        print(username,password)
 
-        user=authenticate(username,password)
+        user=authenticate(username=username,password=password)
+        print('user auth done')
 
         if user is not None:
-            login(user)
-            redirect('')
+            print('in if')
+            login(request, user)
+            return redirect('/')
         else:
+            print('in else')
             return render(request, 'loginUser.html')
 
     return render(request, 'loginUser.html')
 
 def logoutUser(request):
     logout(request)
-    return redirect('loginUser')
+    return redirect('/loginUser')
